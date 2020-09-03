e8d397b0f299d4d0f4ffb998a2eabe16cd476bb7
krsivanov <krsivanov@gmail.com>
2020-09-03 14:27:14 +0300

    Update textpro.py

--- textpro.py
+++ textpro.py
@@ -1,4 +1,17 @@
 def sentence_maker(phrase):
     interogatives = ("how", "what", "why", "where")
     capitalized = phrase.capitalize()
-    if phrase.startswith()
+    if phrase.startswith(interogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
+
+results =[]
+while True:
+    user_input = input('Say something: ')
+    if user_input == "\end":
+        break
+    else:
+        results.append(sentence_maker(user_input))
+
+print(' '.join(results))
