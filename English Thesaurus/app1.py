a1c945560234ffcbd1c26b99ecb239b077a2570c
krsivanov <krsivanov@gmail.com>
2020-09-13 09:53:19 +0300

    Update app1.py

--- English Thesaurus/app1.py
+++ English Thesaurus/app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
@@ -6,9 +7,13 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w,data.keys()))>0:
+        return 'Did you mean %s instead?' % get_close_matches(w,data.keys())[0]
     else:
         return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
+#get_close_matches(word,data.keys())[0]
+
 
 print(translate(word))
